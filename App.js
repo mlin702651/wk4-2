3a19a0fdc47f993f80533a9baaa15548c1e6b7f0
Ding <mlin702651@gmail.com>
2020-04-20 21:28:23 +0800

    drawer改

--- App.js
+++ App.js
@@ -1,23 +1,158 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,  } from '@react-navigation/drawer';
+import { Tile } from 'react-native-elements';
 import { StyleSheet, Text, View, Image, FlatList} from 'react-native';
 import Header from './js/Header';
 import Contain from './js/Contain';
 import Containlist from './list.json';
 import Bottombar from './js/Bottombar';
-import Drawer from './js/Drawer';
+import MeScreen from './js/Drawer';
 
+const Drawer = createDrawerNavigator();
 
-export default function App() {
+const AlbumStack = () => {
   return (
     <View style={styles.container}>
-      <Drawer />
-      <Header />
-      <FlatList
-        data={Containlist}
-        renderItem={({ item }) => <Contain list={item} />}
-        keyExtractor={item => item.title} />
-    <Bottombar/>
-    </View>
+    <Header />
+    <FlatList
+      data={Containlist}
+      renderItem={({ item }) => <Contain list={item} />}
+      keyExtractor={item => item.title} />
+  <Bottombar/>
+  </View>
+);
+}
+
+const CustomDrawerAnimatedContent = ({ progress, ...rest }) => {
+  const translateX = Animated.interpolate(progress, {
+    inputRange: [0, 1],
+    outputRange: [-100, 0],
+  });
+
+  return (
+    <DrawerContentScrollView {...rest}>
+      <Animated.View style={{ transform: [{ translateX }] }}>
+        <DrawerItemList {...rest} />
+        
+      </Animated.View>
+    </DrawerContentScrollView>
+  );
+}
+
+const CustomDrawerContent = ({ progress, ...rest }) => {
+  return (
+    <DrawerContentScrollView {...rest}>
+      <Tile
+        imageSrc={{uri:'https://cdn.zeplin.io/5e6edfd117dbf813ddad315b/assets/6FA2E86D-64B3-4F7D-8CA4-9C5F444C0A1B.png'}}
+        containerStyle={{
+          height:240,
+          backgroundColor:'#00b49f',
+          marginTop:-40,
+          paddingTop:60,
+          paddingLeft:20
+        }}
+        imageContainerStyle={{
+          width:100,
+          height:100,
+          marginTop:200
+        }}
+        featured
+      />
+      <View>
+        <Text style={{ fontSize:20,color:'#ffffff',marginTop:-65,marginLeft:26 }}>GamexHCl</Text>
+        <Text style={{ fontSize:20,color:'#ffffff',marginLeft:26 }}>gdlab2017@gmail.com</Text>
+      </View>
+      <DrawerItemList {...rest} />
+    </DrawerContentScrollView>
+  );
+}
+//主體
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator 
+      drawerStyle={
+        {                                                                                                                                                     
+          width: 320,
+        }
+      }
+      drawerContentOptions={{
+        activeBackgroundColor: '#00b49f',
+        activeTintColor:'#fff',
+        itemStyle: { marginLeft: 0, marginRight: 0 },     
+        labelStyle: { fontSize: 20 },
+      }}
+      drawerContent={props => <CustomDrawerContent {...props} />}
+      >
+        <Drawer.Screen 
+        name="Home" 
+        component={AlbumStack} 
+        options={{
+              drawerIcon: ({ tintColor }) => (
+                <Image
+                  source={require('./js/img/icon_drawer_home.png')}
+                  style={{width:36, height: 36, tintColor: tintColor,marginLeft:26 }}
+                />
+                
+              ),
+
+        }}
+        />
+        <Drawer.Screen 
+        name="My Book" 
+        component={AlbumStack} 
+        options={{
+          drawerIcon: ({ tintColor }) => (
+            <Image
+              source={require('./js/img/icon_bottomnav_mybook.png')}
+              style={{width:36, height: 36, tintColor: tintColor,marginLeft:26 }}
+            />
+          ),
+    }}
+        />
+        <Drawer.Screen
+        name="Favorites"
+        component={AlbumStack} 
+        options={{
+          drawerIcon: ({ tintColor }) => (
+            <Image
+              source={require('./js/img/icon_drawer_favorites.png')}
+              style={{width:36, height: 36, tintColor: tintColor,marginLeft:26 }}
+            />
+          ),
+    }}
+          />
+        <Drawer.Screen
+         name="Settings"
+         component={AlbumStack}
+         options={{
+          drawerIcon: ({ tintColor }) => (
+            <Image
+              source={require('./js/img/icon_drawer_setting.png')}
+              style={{width:36, height: 36, tintColor: tintColor,marginLeft:26 }}
+            />
+          ),
+    }}
+         />
+        <Drawer.Screen 
+        name="About us" 
+        component={AlbumStack}
+        options={{
+          drawerIcon: ({ tintColor }) => (
+            <Image
+              source={require('./js/img/icon_drawer_aboutus.png')}
+              style={{width:36, height: 36, tintColor: tintColor,marginLeft:26 }}
+            />
+          ),
+    }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
--- js/Drawer.js
+++ js/Drawer.js
@@ -1,41 +1,73 @@
 import React from 'react';
-import { StyleSheet, Text, View ,Image} from 'react-native';
+import { StyleSheet, Text, View ,Image,ScrollView} from 'react-native';
 
 const Drawer = () => {
   return (
-        <View style={styles.abouslute}>
-            <View style={styles.background}></View>
-                <View style={styles.viewStyle}>
-                    <View style={styles.top}>
-                        <Image style={styles.img1} source={{uri:'https://cdn.zeplin.io/5e6edfd117dbf813ddad315b/assets/6FA2E86D-64B3-4F7D-8CA4-9C5F444C0A1B.png'}}/>
-                        <Text style={styles.name}>GamexHCl</Text>
-                        <Text style={styles.email}>gdlab2017@gmail.com</Text>
-                    </View>
-                    <View style={styles.bottom}>
-                        <View style={styles.box}>
-                            <Image style={styles.Icon} source={require('./img/icon_drawer_home.png')}/>
-                            <Text style={styles.Info}>Home</Text>
-                        </View>
-                        <View style={styles.box}>
-                            <Image style={styles.Icon} source={require('./img/icon_bottomnav_mybook.png')}/>
-                            <Text style={styles.Info}>My Book</Text>
-                        </View>
-                        <View style={styles.box}>
-                            <Image style={styles.Icon} source={require('./img/icon_drawer_favorites.png')}/>
-                            <Text style={styles.Info}>Favorites</Text>
-                        </View>
-                        <View style={styles.box}>
-                            <Image style={styles.Icon} source={require('./img/icon_drawer_setting.png')}/>
-                            <Text style={styles.Info}>Settings</Text>
-                        </View>
-                        <View style={styles.box}>
-                            <Image style={styles.Icon} source={require('./img/icon_drawer_aboutus.png')}/>
-                            <Text style={styles.Info}>About us</Text>
-                        </View>
-                    </View>
-                </View>
-                
+    <ScrollView>
+    <View style={styles.background}></View>
+                 <View style={styles.viewStyle}>
+                     <View style={styles.top}>
+                         <Image style={styles.img1} source={{uri:'https://cdn.zeplin.io/5e6edfd117dbf813ddad315b/assets/6FA2E86D-64B3-4F7D-8CA4-9C5F444C0A1B.png'}}/>
+                         <Text style={styles.name}>GamexHCl</Text>
+                         <Text style={styles.email}>gdlab2017@gmail.com</Text>
+                     </View>
+                     <View style={styles.bottom}>
+                         <View style={styles.box}>
+                             <Image style={styles.Icon} source={require('./img/icon_drawer_home.png')}/>
+                             <Text style={styles.Info}>Home</Text>
+                         </View>
+                         <View style={styles.box}>
+                             <Image style={styles.Icon} source={require('./img/icon_bottomnav_mybook.png')}/>
+                             <Text style={styles.Info}>My Book</Text>
+                         </View>
+                         <View style={styles.box}>
+                             <Image style={styles.Icon} source={require('./img/icon_drawer_favorites.png')}/>
+                             <Text style={styles.Info}>Favorites</Text>
+                         </View>
+                         <View style={styles.box}>
+                             <Image style={styles.Icon} source={require('./img/icon_drawer_setting.png')}/>
+                             <Text style={styles.Info}>Settings</Text>
+                         </View>
+                         <View style={styles.box}>
+                             <Image style={styles.Icon} source={require('./img/icon_drawer_aboutus.png')}/>
+                             <Text style={styles.Info}>About us</Text>
+                         </View>
+                     </View>
         </View>
+    </ScrollView>
+        // <View style={styles.abouslute}>
+        //     <View style={styles.background}></View>
+        //         <View style={styles.viewStyle}>
+        //             <View style={styles.top}>
+        //                 <Image style={styles.img1} source={{uri:'https://cdn.zeplin.io/5e6edfd117dbf813ddad315b/assets/6FA2E86D-64B3-4F7D-8CA4-9C5F444C0A1B.png'}}/>
+        //                 <Text style={styles.name}>GamexHCl</Text>
+        //                 <Text style={styles.email}>gdlab2017@gmail.com</Text>
+        //             </View>
+        //             <View style={styles.bottom}>
+        //                 <View style={styles.box}>
+        //                     <Image style={styles.Icon} source={require('./img/icon_drawer_home.png')}/>
+        //                     <Text style={styles.Info}>Home</Text>
+        //                 </View>
+        //                 <View style={styles.box}>
+        //                     <Image style={styles.Icon} source={require('./img/icon_bottomnav_mybook.png')}/>
+        //                     <Text style={styles.Info}>My Book</Text>
+        //                 </View>
+        //                 <View style={styles.box}>
+        //                     <Image style={styles.Icon} source={require('./img/icon_drawer_favorites.png')}/>
+        //                     <Text style={styles.Info}>Favorites</Text>
+        //                 </View>
+        //                 <View style={styles.box}>
+        //                     <Image style={styles.Icon} source={require('./img/icon_drawer_setting.png')}/>
+        //                     <Text style={styles.Info}>Settings</Text>
+        //                 </View>
+        //                 <View style={styles.box}>
+        //                     <Image style={styles.Icon} source={require('./img/icon_drawer_aboutus.png')}/>
+        //                     <Text style={styles.Info}>About us</Text>
+        //                 </View>
+        //             </View>
+        //         </View>
+                
+        // </View>
     
   );
 };
